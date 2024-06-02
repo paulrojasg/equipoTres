2c1e59bd3517100885070394cd6e60206e8b3ff4
Van <45314803+vantwot@users.noreply.github.com>
2024-06-02 17:12:26 -0500

    Update lista de tareas

---
+++ app/src/main/java/com/example/project2/view/viewholder/TaskViewHolder.kt
@@ -0,0 +1,24 @@
+package com.example.project2.view.viewholder
+
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project2.R
+import com.example.project2.databinding.CardTaskBinding
+import com.example.project2.model.TodoTask
+
+class TaskViewHolder(binding: CardTaskBinding, navController: NavController) :
+    RecyclerView.ViewHolder(binding.root) {
+    val bindingTask = binding
+    val navController = navController
+    fun setCardAppointment(task: TodoTask) {
+        bindingTask.taskName.text = task.name
+
+        bindingTask.itemCardView.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putSerializable("clave", task)
+            //navController.navigate(R.id.action_homeFragment_to_detailFragment, bundle)
+        }
+
+    }
+}
