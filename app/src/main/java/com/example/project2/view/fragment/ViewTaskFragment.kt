04012992c51a04f3a91e4bbacff7bed619322ab8
Nicol_O <nicol.ortiz@correounivalle.edu.co>
2024-06-03 07:51:07 -0500

    Delete and edit implementation was added, also validations

--- app/src/main/java/com/example/project2/view/fragment/ViewTaskFragment.kt
+++ app/src/main/java/com/example/project2/view/fragment/ViewTaskFragment.kt
@@ -12,13 +12,16 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project2.R
+import com.example.project2.databinding.CardTaskBinding
 import com.example.project2.databinding.FragmentViewtaskBinding
+import com.example.project2.model.TodoTask
 import com.example.project2.view.adapter.TaskAdapter
 import com.example.project2.viewmodel.TodoTaskViewModel
 
 class ViewTaskFragment : Fragment() {
 
     private lateinit var binding: FragmentViewtaskBinding
+    private lateinit var binding_card: CardTaskBinding
     private val app: TodoTaskViewModel by viewModels()
 
     override fun onCreateView(
@@ -26,6 +29,7 @@ class ViewTaskFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentViewtaskBinding.inflate(inflater)
+        binding_card = CardTaskBinding.inflate(inflater)
         binding.lifecycleOwner = this
         return binding.root
     }
@@ -59,23 +63,25 @@ override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         observerProgress()
     }
 
-private fun observerListTask(){
-    app.listTodoTask.observe(viewLifecycleOwner) { listTodoTask ->
-        Log.d("Lista de tareas observadas:", listTodoTask.toString())
-        val recycler = binding.recyclerview
-        val layoutManager = LinearLayoutManager(context)
-        recycler.layoutManager = layoutManager
-        val adapter = TaskAdapter(listTodoTask, findNavController())
-        recycler.adapter = adapter
-        adapter.notifyDataSetChanged()
-        Log.d("RecyclerView", "Adapter Set with ${listTodoTask.size} tasks")
+    private fun observerListTask(){
+        app.listTodoTask.observe(viewLifecycleOwner) { listTodoTask ->
+            Log.d("Lista de tareas observadas:", listTodoTask.toString())
+            val recycler = binding.recyclerview
+            val layoutManager = LinearLayoutManager(context)
+            recycler.layoutManager = layoutManager
+            val adapter = TaskAdapter(listTodoTask, findNavController())
+            recycler.adapter = adapter
+            adapter.notifyDataSetChanged()
+            Log.d("RecyclerView", "Adapter Set with ${listTodoTask.size} tasks")
+        }
     }
-}
-
 
     private fun observerProgress(){
         app.progressState.observe(viewLifecycleOwner){status ->
             binding.progress.isVisible = status
         }
     }
+
 }
+
+
