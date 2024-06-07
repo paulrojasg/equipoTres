8589d9f446363a7a9d85ad67daae5a726993644f
Juan Pablo <carabali.juan@correounivalle.edu.co>
2024-06-07 15:57:14 -0500

    Dagger hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-
-    // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
+    id("com.google.dagger.hilt.android") // Aplicar el plugin de Hilt
+    kotlin("kapt")
 }
 
 android {
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
     implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
     implementation("com.google.android.gms:play-services-auth:20.5.0")
-    implementation ("com.firebaseui:firebase-ui-firestore:8.0.2")
+    implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,24 +67,24 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:$fragment_version")
 
     //User interface components
-    val materialVersion= "1.4.0"
+    val materialVersion = "1.4.0"
     implementation("com.google.android.material:material:$materialVersion")
 
     // LiveData
     val liveDataVersion = "2.3.1"
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$liveDataVersion")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$liveDataVersion")
-    implementation ("androidx.lifecycle:lifecycle-common-java8:$liveDataVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$liveDataVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$liveDataVersion")
+    implementation("androidx.lifecycle:lifecycle-common-java8:$liveDataVersion")
 
     // Kotlin Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
     //Glide
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 
     // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0")) 
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
 
     //lottiejson
     val lottieVersion = "3.4.0"
@@ -95,4 +95,12 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 
+    // Dagger Hilt
+    val hiltVersion = "2.45"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+}
+
+kapt {
+    correctErrorTypes = true
 }
