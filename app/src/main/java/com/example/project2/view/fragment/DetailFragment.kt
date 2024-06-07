c17e02848583604444ddb004cb6f7b685f1e158c
Juan Pablo <carabali.juan@correounivalle.edu.co>
2024-06-07 06:48:10 -0500

    Cambios para implementar la vista

--- app/src/main/java/com/example/project2/view/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/project2/view/fragment/DetailFragment.kt
@@ -12,11 +12,6 @@ import com.example.project2.databinding.FragmentDetailBinding
 import com.example.project2.model.TodoTask
 import com.example.project2.viewmodel.TodoTaskViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
     private val app: TodoTaskViewModel by viewModels()
@@ -26,35 +21,31 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDetailBinding.inflate(inflater)
+        // Inflate the layout using View Binding
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        controladores()
         catchIncomingData()
     }
 
-    private fun controladores() {
-        //Lógica para la flecha de atrás
-    }
-
-    private fun catchIncomingData(){
+    private fun catchIncomingData() {
         val receivedBundle = arguments
         receivedTask = receivedBundle?.getSerializable("clave") as TodoTask
-        // Cargar la imagen desde la URL
-        //Editar las variables
-//        Glide.with(this)
-//            .load(receivedTask.imagePath)
-//            .into(binding.petBreedImage)
-//        binding.numberAppointmnet.text = "#${receivedAppointment.id.toString()}"
-//        binding.titleTextDetailsName.text = receivedAppointment.name_pet
-//        binding.petBreedName.text = receivedAppointment.breed
-//        binding.ownerPhone.text = "Telefono: ${receivedAppointment.phone_number}"
-//        binding.ownerName.text = "Propietario: ${receivedAppointment.name_owner}"
-//        binding.petSymptoms.text = receivedAppointment.symptoms
-    }
 
+
+        Log.d("DetailFragment", "Image URL: ${receivedTask.imagePath}") // Verifica la URL en los logs
+
+        // Cargar la imagen desde la URL en el ImageView
+        Glide.with(this)
+            .load(receivedTask.imagePath)
+            .into(binding.flowerImage)
+
+        // Ejemplos de cómo editar las variables
+        binding.tvTaskName.text = receivedTask.name
+        binding.tvTaskDescription.text = receivedTask.description
+    }
 }
