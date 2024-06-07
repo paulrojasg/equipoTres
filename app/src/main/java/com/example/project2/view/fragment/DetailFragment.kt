f9b05191de587be0aae9dbc5914bb2b2eeeeae8a
Juan Pablo <carabali.juan@correounivalle.edu.co>
2024-06-07 13:43:54 -0500

    H.U 5.0 completada.

--- app/src/main/java/com/example/project2/view/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/project2/view/fragment/DetailFragment.kt
@@ -1,12 +1,15 @@
 package com.example.project2.view.fragment
 
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.project2.databinding.FragmentDetailBinding
@@ -30,11 +33,20 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         controladores()
+        setToolbar()
         catchIncomingData()
     }
 
     private fun controladores() {
         // Lógica para la flecha de atrás
+    }
+
+    private fun setToolbar() {
+        val toolbar: Toolbar = binding.contentToolbar.toolbarDetail
+        toolbar.setNavigationOnClickListener {
+            // Navegar a otro fragmento cuando se hace clic en el icono de navegación
+            findNavController().popBackStack()
+        }
     }
 
     private fun catchIncomingData() {
@@ -47,13 +59,15 @@ class DetailFragment : Fragment() {
         Glide.with(this)
             .load("https://loremflickr.com/${receivedTask.imagePath}")
             .transform(CircleCrop())
-        //    .placeholder(R.drawable.placeholder_image)
-        //    .error(R.drawable.error_image)
             .into(binding.flowerImage)
 
         // Ejemplos de cómo editar las variables
         binding.tvTaskName.text = receivedTask.name
-        binding.tvTaskDescription.text = receivedTask.description
+
+        // Convierte la descripción a Editable antes de establecer el texto
+        val editableDescription: Editable = Editable.Factory.getInstance().newEditable(receivedTask.description)
+        binding.tvTaskDescription.text = editableDescription
+
         binding.tvTaksPriority.text = receivedTask.priority
         binding.TvTaksCategory.text = receivedTask.category
     }
