c22fad05af050dbabc1d29c4b8319fd2e0210c4b
Nicol Ortiz <nicol.ortiz@correounivalle.edu.co>
2024-06-07 14:44:50 -0500

    Erasing unnecessary things

--- app/src/main/java/com/example/project2/view/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/project2/view/fragment/DetailFragment.kt
@@ -8,17 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.project2.databinding.FragmentDetailBinding
 import com.example.project2.model.TodoTask
-import com.example.project2.viewmodel.TodoTaskViewModel
 
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
-    private val app: TodoTaskViewModel by viewModels()
     private lateinit var receivedTask: TodoTask
 
     override fun onCreateView(
